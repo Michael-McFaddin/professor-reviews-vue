c1dd78504d9436614c7aee1264e567b30e5c714f
mike <mikemcfaddin@gmail.com>
2020-05-26 17:52:55 -0400

    adds initial vue setup w/professors index

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,53 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div v-for="professor in professors">
+      <h1 v-on:click="showProfessor(professor)">{{ professor.name }}</h1>
+      <div v-if="professor === currentProfessor">
+        <h3>Tile: {{ professor.title }}</h3>
+        <h3>School: {{ professor.school }}</h3>
+        <h3>Department: {{ professor.department }}</h3>
+        <div v-for='review in reviews' v-if='review.professor_id === currentProfessor.id'>
+          <h4>Review: {{ review.text }}</h4>
+          <h4>Rating: {{ review.rating }}</h4>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
+
+<style>
+</style>
+
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  data: function() {
+    return {
+      professors: [],
+      currentProfessor: {},
+      reviews: [],
+      test: ""
+    };
+  },
+
+  created: function() {
+    axios.get('/professors').then(response => {
+      this.professors = response.data;
+    })
+    axios.get('/reviews').then(response => {
+      this.reviews = response.data;
+    })
+  },
+  methods: {
+    showProfessor: function(inputProfessor) {
+      if(this.currentProfessor !== inputProfessor) {
+        this.currentProfessor = inputProfessor}
+        else {
+          this.currentProfessor = {};
+        }
+    }
   }
 }
 </script>
